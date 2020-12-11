5a9272ca017dbf8491df96b6c285b046335e4361
Dionne Stratton <mylife33@hotmail.com>
2020-12-11 11:29:07 -0600

    test seeds created and run

---
+++ data/seeds/01-projects.js
@@ -0,0 +1,19 @@
+exports.seed = function (knex, Promise) {
+  return knex("projects").insert([
+    {
+      project_name: "project1",
+      project_description: "testing",
+      completed: 0,
+    },
+    {
+      project_name: "project2",
+      project_description: "testing",
+      completed: 0,
+    },
+    {
+      project_name: "project3",
+      project_description: "testing",
+      completed: 0,
+    },
+  ]);
+};

b337cf1701855981534d6d39f5331c45049c5d65
Dionne Stratton <mylife33@hotmail.com>
2020-12-11 16:59:18 -0600

    removed 4th table

--- data/migrations/20201211102533_create-tables.js
+++ data/migrations/20201211102533_create-tables.js
@@ -24,26 +24,26 @@ exports.up = function (knex) {
         .inTable("projects")
         .onDelete("CASCADE")
         .onUpdate("CASCADE");
-    })
-    .createTable("project_resources", (tbl) => {
-      tbl.increments();
-      tbl
-        .integer("project_id")
-        .unsigned()
-        .notNullable()
-        .references("id")
-        .inTable("projects")
-        .onDelete("CASCADE")
-        .onUpdate("CASCADE");
-      tbl
-        .integer("resource_id")
-        .unsigned()
-        .notNullable()
-        .references("id")
-        .inTable("resources")
-        .onDelete("CASCADE")
-        .onUpdate("CASCADE");
     });
+  // .createTable("project_resources", (tbl) => {
+  //   tbl.increments();
+  //   tbl
+  //     .integer("project_id")
+  //     .unsigned()
+  //     .notNullable()
+  //     .references("id")
+  //     .inTable("projects")
+  //     .onDelete("CASCADE")
+  //     .onUpdate("CASCADE");
+  //   tbl
+  //     .integer("resource_id")
+  //     .unsigned()
+  //     .notNullable()
+  //     .references("id")
+  //     .inTable("resources")
+  //     .onDelete("CASCADE")
+  //     .onUpdate("CASCADE");
+  // });
 };
 exports.down = function (knex) {
   return knex.schema
